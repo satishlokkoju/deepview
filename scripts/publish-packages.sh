36266d92deaad6cc639196d75485287aab4fc3d7
Satish Lokkoju <satish.lokkoju@gmail.com>
2025-01-09 11:28:01 -0800

    Removed the cashing mechanism and fixed minor typos

--- scripts/publish-packages.sh
+++ scripts/publish-packages.sh
@@ -1,9 +1,24 @@
 # BEFORE DEPLOY: Update the python package versions in Canvas and each widget's _version.py file.
+python3 -m pip install twine
+python3 -m pip install build
+(cd src/deepview; rm -rf dist; python3 -m build;python3 -m twine upload --repository pypi dist/* )
+(cd src/deepview_tensorflow; rm -rf dist; python3 -m build;python3 -m twine upload --repository pypi dist/* )
+(cd src/deepview_data; rm -rf dist; python3 -m build;python3 -m twine upload --repository pypi dist/* )
+(cd src/deepview_torch; rm -rf dist; python3 -m build;python3 -m twine upload --repository pypi dist/* )
 
+cd src/deepview_canvas
 yarn
 yarn workspace @betterwithdata/canvas_viz build
-(cd canvas_ux; rm -rf dist; yarn build; python setup.py bdist_wheel; python setup.py sdist; twine upload dist/*)
-
+(cd canvas_ux; rm -rf dist; yarn build:prod; python3 setup.py bdist_wheel; python3 setup.py sdist;python3 -m twine upload --repository pypi dist/* )
 for d in widgets/* ; do
-	(cd $d; yarn build; rm -rf dist; python setup.py bdist_wheel; python setup.py sdist; twine upload dist/*)
+    # Skip if d is __init__.py or __pycache__
+    if [ "$d" = "widgets/__init__.py" ] || [ "$d" = "widgets/__pycache__" ]; then
+        continue
+    fi
+
+    # Check if the directory has a setup.py and package.json
+    if [ -f "$d/setup.py" ] && [ -f "$d/package.json" ]; then
+        (
+            cd "$d"; rm -rf dist; yarn build:prod; python3 setup.py bdist_wheel; python3 setup.py sdist;python3 -m twine upload --repository pypi dist/* )
+    fi
 done
